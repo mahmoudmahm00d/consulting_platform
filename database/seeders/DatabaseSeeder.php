a4afdbfed7869f1ec90852b3966c0a0f464b41ff
mahmoudmahm00d <mahmoudmahm00d@protonmail.com>
2022-12-17 16:24:05 +0300

    Add default users to database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        if (Role::count() == 0) {
+            Role::create([
+                'name' => 'Admin'
+            ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            Role::create([
+                'name' => 'Specialist'
+            ]);
+
+            Role::create([
+                'name' => 'User'
+            ]);
+        }
+
+        if (User::count() == 0) {
+            $admin = User::create([
+                'first_name' => 'Super',
+                'last_name' => 'Admin',
+                'phone_number' => '1500400300',
+                'email' => 'admin@consulting.com',
+                'password' => Hash::make('AdminP@$$w0rd!'),
+            ]);
+
+            $admin->assignRole('Admin');
+
+            $user = User::create([
+                'first_name' => 'Guest',
+                'phone_number' => '1300400500',
+                'email' => 'guest@consulting.com',
+                'password' => Hash::make('AdminP@$$w0rd!'),
+            ]);
+
+            $user->assignRole('User');
+
+        }
     }
 }
