68ebad1d946b785984493eb958fa2edcff161dee
mahmoudmahm00d <mahmoudmahm00d@protonmail.com>
2023-02-15 22:49:41 +0300

    Modify database seeder
    - Seed users with wallets
    - Seed categories with images
    - Seed more contact info types with icons

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\ContactInfoType;
 use App\Models\User;
+use App\Models\Wallet;
+use ApplicationRoles;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
@@ -22,17 +24,17 @@ class DatabaseSeeder extends Seeder
     {
         if (Role::count() == 0) {
             Role::create([
-                'name' => 'Admin',
+                'name' => ApplicationRoles::$admin,
                 'guard_name' => 'web'
             ]);
 
             Role::create([
-                'name' => 'Specialist',
+                'name' => ApplicationRoles::$specialist,
                 // 'guard_name' => 'api'
             ]);
 
             Role::create([
-                'name' => 'User',
+                'name' => ApplicationRoles::$user,
                 // 'guard_name' => 'api'
             ]);
         }
@@ -54,34 +56,80 @@ class DatabaseSeeder extends Seeder
                 'password' => Hash::make('Password'),
             ]);
 
-            $user_role = Role::findByName('User');
-            
-            $user->assignRole($user_role);
+            $user->assignRole(Role::findByName(ApplicationRoles::$user));
+
+            Wallet::create([
+                'user_id' => $user->id,
+                'amount' => 5000,
+            ]);
+
+
+            $specialist = User::create([
+                'name' => 'Specialist',
+                'phone_number' => '1200300400',
+                'email' => 'specialist@consulting.com',
+                'password' => Hash::make('Specialist'),
+            ]);
+
+            $specialist->assignRole(Role::findByName(ApplicationRoles::$specialist));
+
+            Wallet::create([
+                'user_id' => $specialist->id,
+                'amount' => 5000,
+            ]);
         }
 
-        if(Category::count() == 0)
-        {
+        if (Category::count() == 0) {
             Category::create([
                 'name' => 'Medical',
+                'image' => '/images/medical.png'
             ]);
 
             Category::create([
                 'name' => 'Programming',
+                'image' => '/images/programming.png',
             ]);
         }
 
-        if(ContactInfoType::count() == 0)
-        {
+        if (ContactInfoType::count() == 0) {
+            ContactInfoType::create([
+                'name' => 'Address',
+                'description' => 'Physical Address',
+                'icon' => '/images/address.png',
+            ]);
+
+            ContactInfoType::create([
+                'name' => 'Telephone',
+                'description' => 'Telephone',
+                'icon' => '/images/phone.png',
+            ]);
+
             ContactInfoType::create([
                 'name' => 'Whatsapp',
                 'url' => 'https://api.whatsapp.com/send/?&text&app_absent=0&phone=',
-                'description' => 'Whatsapp account'
+                'description' => 'Whatsapp account',
+                'icon' => '/images/whatsapp.png',
+            ]);
+
+            ContactInfoType::create([
+                'name' => 'Github',
+                'url' => 'https://github.com/',
+                'description' => 'Github account',
+                'icon' => '/images/github.png',
             ]);
 
             ContactInfoType::create([
                 'name' => 'Facebook',
                 'url' => 'https://www.facebook.com/',
-                'description' => 'Facebook username'
+                'description' => 'Facebook username',
+                'icon' => '/images/facebook.png',
+            ]);
+
+            ContactInfoType::create([
+                'name' => 'Twitter',
+                'url' => 'https://www.twitter.com/',
+                'description' => 'Twitter username',
+                'icon' => '/images/twitter.png',
             ]);
         }
     }
