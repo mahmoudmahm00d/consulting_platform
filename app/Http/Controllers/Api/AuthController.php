abfdbdb9d312bc8b0e3583a278f8e39f41a07814
mahmoudmahm00d <mahmoudmahm00d@protonmail.com>
2023-02-15 22:33:00 +0300

    Update auth API controller
    - Return appropriate response messages

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,13 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use PHPOpenSourceSaver\JWTAuth\JWTAuth;
-use Illuminate\Support\Str;
-use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class AuthController extends Controller
 {
@@ -21,7 +18,6 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth.refresh', ['only' => ['refresh']]);
         $this->middleware('auth:api', ['except' => ['login', 'register', 'refresh']]);
     }
 
@@ -42,7 +38,16 @@ class AuthController extends Controller
         $user = User::create($fields);
         $user->assignRole($role['role']);
 
-        return response()->json('', 201);
+        Wallet::create([
+            'amount' => 0,
+            'user_id' => $user->id
+        ]);
+
+        return response()->json([
+            'code' => 200,
+            'status' => 'success',
+            'message' => 'Account created successfully'
+        ]);
     }
 
     /**
@@ -57,11 +62,19 @@ class AuthController extends Controller
         $userExists = Auth::attempt($credentials);
 
         if (!$userExists) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json([
+                'code' => 401,
+                'status' => 'error',
+                'message' => 'Wrong email or password'
+            ], 401);
         }
 
         $user = User::with('roles')->where('email', '=', $credentials['email'])->first();
-        $token = auth('api')->claims(['role' => $user->roles[0]->name])->tokenById($user->id);
+        // Add user role claim
+        $token = auth('api')
+            ->claims(['role' => $user->roles[0]->name])
+            ->tokenById($user->id);
+
         return $this->respondWithToken($token, $user->roles[0]->name);
     }
 
@@ -74,40 +87,23 @@ class AuthController extends Controller
     {
         auth('api')->logout();
 
-        return response()->json(['message' => 'Successfully logged out']);
+        return response()->json([
+            'code' => 200,
+            'status' => 'success',
+            'message' => 'Successfully logged out'
+        ]);
     }
 
-    public function refresh() {
-        return $this->createNewToken(auth('api')->refresh());
+    public function refresh()
+    {
+        $user = User::with('roles')->find(auth('api')->id);
+        // Add user role claim
+        $token = auth('api')
+            ->claims(['role' => $user->roles[0]->name])
+            ->tokenById($user->id);
+
+        return $this->respondWithToken($token, $user->roles[0]->name);
     }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    // public function refresh()
-    // {
-        // return $this->respondWithToken(auth('api'));
-        // try {
-        //     $token = $this->auth->parseToken()->refresh();
-        // } catch (JWTException $e) {
-        //     return response()->json($e);
-        //     throw new UnauthorizedHttpException('jwt-auth', $e->getMessage(), $e, $e->getCode());
-        // }
-
-        // try {
-        //     $payload = auth('api')->payload();
-        //     $token = auth('api')->refresh();
-        //     $user = User::with('roles')->find($payload['sub']);
-        //     return $this->respondWithToken($token, $user->roles[0]->name);
-        // } catch (\PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException $th) {
-        //     return response()->json([
-        //         'status' => 'error',
-        //         'message' => 'Token expired'
-        //     ]);
-        // }
-    // }
 
     /**
      * Get the token array structure.
@@ -120,10 +116,14 @@ class AuthController extends Controller
     {
 
         return response()->json([
-            'access_token' => $token,
-            'role' => $role,
-            'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL(),
+            'code' => 200,
+            'status' => 'success',
+            'data' => [
+                'access_token' => $token,
+                'role' => $role,
+                'token_type' => 'bearer',
+                'expires_in' => auth('api')->factory()->getTTL() * 1,
+            ]
         ]);
     }
 }
