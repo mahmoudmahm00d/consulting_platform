0796737a84eb569d3b7971ee8439712b25d74997
mahmoudmahm00d <mahmoudmahm00d@protonmail.com>
2023-02-15 22:31:45 +0300

    Update account API controller
    - Implement actions' logic
    - Return appropriate response
    - Add more validation rules
    - Add the following actions:
        * schedule
        * delete schedule

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -4,17 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Api\SpecialistUser;
-use App\Models\Api\ContactInfo as ApiContactInfo;
-use App\Models\Api\Specialize as ApiSpecialize;
-use App\Models\Category;
 use App\Models\ContactInfo;
 use App\Models\ContactInfoType;
+use App\Models\Schedule;
 use App\Models\Specialize;
 use App\Models\User;
+use ApplicationRoles;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Throwable;
+use Illuminate\Support\Facades\Validator;
 
 class AccountController extends Controller
 {
@@ -25,54 +23,81 @@ class AccountController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api');
-        // $this->middleware('auth.jwt');
+        $this->middleware('auth:api')->except('testing');
+        $this->middleware('role:Specialist')->except([
+            'me',
+            'update',
+            'changeEmail',
+            'changePassword',
+            'changePhone',
+        ]);
     }
 
     public function addSpecializes(Request $request)
     {
         $user = auth('api')->user();
-        if (!$user) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $specializes = $request->specializes;
+
         try {
             foreach ($specializes as $specialize) {
+                $validator = Validator::make($specialize, [
+                    'name' => 'required',
+                    'category' => ['required', 'numeric'],
+                    'price' => ['required', 'numeric'],
+                ]);
+
+                if ($validator->fails()) {
+                    return response()->json([
+                        'message' => 'Bad request',
+                        'errors' => $validator->errors()
+                    ], 422);
+                }
+
                 Specialize::create([
                     'name' => $specialize['name'],
                     'description' => $specialize['description'],
                     'user_id' => $user->id,
+                    'price' => $specialize['price'],
                     'category_id' => $specialize['category'],
                 ]);
             }
         } catch (\Throwable $ex) {
-            return response()->json(['message' => 'An error was thrown when creating specialize'], 400);
+            return response()->json([
+                'status' => 'error',
+                'code' => 500,
+                'message' => 'An error was thrown when creating specialize'
+            ], 500);
         }
 
         return response()->json([
-            'message' => 'success',
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Specializes created successfully',
         ]);
     }
 
     public function addContacts(Request $request)
     {
         $user = auth('api')->user();
-        if (!$user) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $contacts = $request->contacts;
 
         foreach ($contacts as $contact) {
+            $validator = Validator::make($contact, [
+                'contact_info' => 'required',
+                'type' => ['required', 'numeric'],
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Bad request',
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+
             if (!ContactInfoType::find($contact['type'])) {
                 return response()->json([
+                    'status' => 'error',
+                    'code' => 400,
                     'message' => 'The type ' . $contact['type'] . 'not found'
                 ], 400);
             }
@@ -87,54 +112,60 @@ class AccountController extends Controller
                 ]);
             }
         } catch (\Throwable $ex) {
-            return response()->json(['message' => 'An error was thrown when creating contacts'], 400);
+            return response()->json([
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'An error was thrown when creating contacts'
+            ], 400);
         }
 
         return response()->json([
-            'message' => 'success',
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Contacts Added successfully',
         ]);
-
-        return response()->json();
     }
 
     public function deleteSpecialize($id)
     {
         $user = auth('api')->user();
-        if (!$user) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $specialize = Specialize::where('id', '=', $id)->where('user_id', '=', $user->id)->first();
 
         if (!$specialize) {
-            return response()->json([], 404);
+            return response()->json([
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Specialize not found'
+            ], 404);
         }
 
         $specialize->delete();
-        return response()->json([], 204);
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'message' => "Specialize #$id deleted successfully"
+        ]);
     }
 
     public function deleteContact($id)
     {
         $user = auth('api')->user();
-        if (!$user) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $contact = ContactInfo::where('id', '=', $id)->where('user_id', '=', $user->id)->first();
 
         if (!$contact) {
-            return response()->json([], 404);
+            return response()->json([
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Contact not found'
+            ], 404);
         }
 
         $contact->delete();
-        return response()->json([], 204);
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'message' => "Contact $id deleted successfully"
+        ]);
     }
 
     public function uploadImage(Request $request)
@@ -142,98 +173,213 @@ class AccountController extends Controller
         if (!$request->has('profile_image')) {
             return response()->json([
                 'status' => 'error',
+                'code' => 400,
                 'message' => 'Image is required'
             ], 400);
         }
 
         $fields = $request->validate(['profile_image' => ['file', 'max:512']]);
         $user = User::find(auth('api')->user()->id);
+
         $user->profile_image = $request->file('profile_image')->store('profile', 'public');
         $user->update();
 
         return response()->json([
             'status' => 'success',
+            'code' => 200,
             'message' => 'Image uploaded successfully'
+        ]);
+    }
+
+    public function schedule()
+    {
+        $user = auth('api')->user();
+        $schedule = SpecialistUser::find($user->id)->schedule;
+
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'data' => $schedule ? $schedule : []
         ]);
     }
 
     // Schedule
     public function addSchedule(Request $request)
     {
-        $schedules = $request->schedules;
-        if (!$schedules) {
-            return response()->json(['message' => 'Schedule is required'], 400);
+        $user = auth('api')->user();
+        $schedule = $request->schedule;
+
+        if (!$schedule) {
+            return response()->json([
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Schedule is required'
+            ], 400);
         }
 
-        foreach ($schedules as $item) {
+        foreach ($schedule as $item) {
+            // Insure weekday is valid day
             if ($item['day'] < 0 || 7 < $item['day']) {
-                return response()->json(['message' => 'Invalid day'], 400);
+                return response()->json([
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'Invalid day'
+                ], 400);
+            }
+            // Insure from < to 
+            if ($item['from'] >= $item['to']) {
+                return response()->json([
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'To time must be bigger than from time'
+                ], 400);
+            }
+
+            $condition1 = Schedule::whereTime('from', '<=', $item['from'])
+                ->whereTime('to', '>=', $item['to'])
+                ->where('user_id', $user->id)
+                ->where('day', $item['day'])
+                ->count();
+
+            $condition2 = Schedule::whereTime('from', '<=', $item['to'])
+                ->whereTime('to', '>=', $item['to'])
+                ->where('user_id', $user->id)
+                ->where('day', $item['day'])
+                ->count();
+
+            $condition3 = Schedule::whereTime('from', '>=', $item['from'])
+                ->whereTime('to', '<=', $item['to'])
+                ->where('user_id', $user->id)
+                ->where('day', $item['day'])
+                ->count();
+
+            if ($condition1 && $condition2) {
+                return response()->json([
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'Schedule in other schedule'
+                ], 400);
+            }
+
+            if (($condition1 && !$condition2) // One in 
+                || (!$condition1 && $condition2) // and other out
+            ) {
+                return response()->json([
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'Schedule in between other schedule'
+                ], 400);
+            }
+
+            if ($condition3) {
+                return response()->json([
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'Schedule contains other schedule'
+                ], 400);
+            }
+
+            // Create schedule
+            try {
+                Schedule::updateOrCreate([
+                    'day' => $item['day'],
+                    'from' => $item['from'],
+                    'to' => $item['to'],
+                    'user_id' => $user->id,
+                ]);
+            } catch (\Throwable $th) {
+                return response()->json([
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'An error occurs while creating schedule',
+                    'data' => $item,
+                ], 400);
             }
         }
 
-        return response()->json([], 204);
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Schedule created successfully'
+        ]);
+    }
+
+    public function deleteSchedule($id)
+    {
+        $user = auth('api')->user();
+        $schedule = Schedule::where('user_id', '=', $user->id)->where('id', '=', $id)->first();
+
+        if (!$schedule) {
+            return response()->json([
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Schedule not found'
+            ], 404);
+        }
+
+        $schedule->delete();
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'message' => "Schedule $id deleted successfully"
+        ]);
     }
 
     // Profile
     public function me()
     {
-        $user = auth('api')->user();
-        if (!$user) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
+        $user = User::find(auth('api')->user()->id);
+
+        if ($user->hasRole(ApplicationRoles::$specialist)) {
+
+            $user = SpecialistUser::with(['categories' => function ($query) {
+                $query->distinct('id');
+            }])->with(['ratings' => function ($query) {
+                $query->selectRaw('avg(rating) as average_rating, specialist_id')
+                    ->groupBy('specialist_id');
+            }])->with(['contacts' => function ($query) {
+                $query->with('type');
+            }])->with(['specializes' => function ($query) {
+                $query->with('category');
+            }])->with('schedule')
+                ->find(auth('api')->user()->id);
         }
 
-        $user = SpecialistUser::find($user->id);
-
-        $specializes = ApiSpecialize::with('category')->where('user_id', '=', $user->id)->get();
-        $contacts = ApiContactInfo::with('type')->where('user_id', '=', $user->id)->get();
-        $categories = User::join('specializes', 'specializes.user_id', '=', 'users.id')
-            ->join('categories', 'categories.id', '=', 'specializes.category_id')
-            ->where('user_id', '=', $user->id)
-            ->distinct()
-            ->get(['categories.name']);
-
-        $user['categories'] = $categories;
-        $user['specializes'] = $specializes;
-        $user['contacts'] = $contacts;
-        return response()->json(['message' => 'success', 'data' => $user]);
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'data' => $user
+        ]);
     }
 
     // Update account info
     public function update(Request $request)
     {
-        if (!auth('api')->user()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $fields = $request->validate([
-            'name' => ['required', 'max:256', 'string'],
+            'name' => ['nullable', 'max:256', 'string'],
             'about' => ['nullable'],
         ]);
 
         $user = User::find(auth('api')->user()->id);
-        $user->update($fields);
+        if ($fields['name']) {
+            $user->name = $fields['name'];
+        }
+
+        if ($fields['about']) {
+            $user->about = $fields['about'];
+        }
+        
+        $user->update();
 
         return response()->json([
             'status' => 'success',
+            'code' => 200,
             'message' => 'Account updated successfully'
         ]);
     }
 
     public function changeEmail(Request $request)
     {
-        if (!auth('api')->user()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $fields = $request->validate([
             'email' => ['required', 'max:256', 'email'],
         ]);
@@ -245,8 +391,9 @@ class AccountController extends Controller
         if (!$validPassword) {
             return response()->json([
                 'status' => 'error',
+                'code' => 400,
                 'message' => 'Wrong password'
-            ]);
+            ], 400);
         }
 
         $emailFound = User::where('email', '=', $fields['email'])
@@ -255,27 +402,22 @@ class AccountController extends Controller
         if ($emailFound) {
             return response()->json([
                 'status' => 'error',
+                'code' => 400,
                 'message' => 'This email already used, Use another email'
-            ]);
+            ], 400);
         }
 
         $user->update($fields);
 
         return response()->json([
             'status' => 'success',
+            'code' => 200,
             'message' => 'Email updated successfully'
         ]);
     }
 
     public function changePhone(Request $request)
     {
-        if (!auth('api')->user()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $fields = $request->validate([
             'phone' => ['required', 'max:256', 'email'],
         ]);
@@ -287,8 +429,9 @@ class AccountController extends Controller
         if (!$validPassword) {
             return response()->json([
                 'status' => 'error',
+                'code' => 400,
                 'message' => 'Wrong password'
-            ]);
+            ], 400);
         }
 
         $emailFound = User::where('phone_number', '=', $fields['phone'])
@@ -297,27 +440,22 @@ class AccountController extends Controller
         if ($emailFound) {
             return response()->json([
                 'status' => 'error',
+                'code' => 400,
                 'message' => 'This phone number already used, Use another email'
-            ]);
+            ], 400);
         }
 
         $user->update($fields);
 
         return response()->json([
             'status' => 'success',
+            'code' => 200,
             'message' => 'Phone number updated successfully'
         ]);
     }
 
     public function changePassword(Request $request)
     {
-        if (!auth('api')->user()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
         $fields = $request->validate([
             'password' => ['required'],
             'newPassword' => ['required'],
@@ -330,14 +468,16 @@ class AccountController extends Controller
         if (!$validPassword) {
             return response()->json([
                 'status' => 'error',
+                'code' => 400,
                 'message' => 'Wrong password'
-            ]);
+            ], 400);
         }
 
         $user->update(['password' => Hash::make($fields['newPassword'])]);
 
         return response()->json([
             'status' => 'success',
+            'code' => 200,
             'message' => 'Password updated successfully'
         ]);
     }
