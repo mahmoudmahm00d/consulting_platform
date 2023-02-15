a945e9d57ee99ebdf5d609a11eae74c2e0d86597
mahmoudmahm00d <mahmoudmahm00d@protonmail.com>
2023-02-15 22:26:35 +0300

    Update users web controller
    - Add transaction and deposit actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use App\Models\User;
+use App\Models\Wallet;
+use Illuminate\Http\Request;
+use Intervention\Image\Exception\NotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use TransactionTypes;
 
 class UsersController extends Controller
 {
@@ -18,7 +24,45 @@ class UsersController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::with('roles')->get();
         return view('users.index', ['users' => $users]);
+    }
+
+    public function transaction($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            throw new NotFoundHttpException();
+        }
+
+        return view('users.transaction', [
+            'id' => $id
+        ]);
+    }
+
+    public function deposit(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            throw new NotFoundHttpException();
+        }
+
+        $wallet = Wallet::where('user_id', $id)->first();
+        if (!$wallet) {
+            throw new NotFoundException();
+        }
+
+        $fields = $request->validate([
+            'amount' => 'required'
+        ]);
+
+        Transaction::create([
+            'to_wallet' => $wallet->id,
+            'amount' => $fields['amount'],
+            'type' => TransactionTypes::$deposit
+        ]);
+
+        return redirect('/users');
     }
 }
